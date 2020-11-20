1e6ae6efec97f040643d49230125dee696579c13
double128 <cozmoop@gmail.com>
2020-11-20 16:26:58 -0500

    Uh oh I was a naughty little boy and had a baaaaad gitignore uh oh

--- pox/ext/flow_stats.py
+++ pox/ext/flow_stats.py
@@ -18,55 +18,27 @@ log = core.getLogger()
 # networkx graph object init
 g = None
 
+# The maximum amount of acceptable delay (in ms) on a link - anything above this will result in the link being less optimal
+DELAY_CAP = 200
+
 def _handle_connectionup(event):
     global g
     # Add a node to the networkx graph and assign it an identifiable name
     sw_name = "sw" + str(event.dpid)
     g.add_node(event.dpid, name=sw_name)
-    
+
+def _generate_host_list():
+    global gw
 
 def _timer_func ():
     global g
-
-    # d1 = dpid1; d2 = dpid2; d = attrs as a dict
-    #for d1, d2, k, d in graph.edges(data=True, keys=True):
-    #    conn1 = core.openflow.connections.get(d1)
-    #    conn2 = core.openflow.connections.get(d2)
-    #    print(conn1)
-    #    print(conn2)
-
-
+    hosts = []
+    for 
+    #short_path = g.shortest_path(
     for connection in core.openflow._connections.values():
-        connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
-        connection.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
-    log.debug("Sent %i flow/port stats request(s)", len(core.openflow._connections))
-
-# handler to display flow statistics received in JSON format
-# structure of event.stats is defined by ofp_flow_stats()
-def _handle_flowstats_received (event):
-  stats = flow_stats_to_list(event.stats)
-  log.debug("FlowStatsReceived from %s: %s", 
-    dpidToStr(event.connection.dpid), stats)
-  """
-  # Get number of bytes/packets in flows for web traffic only
-  web_bytes = 0
-  web_flows = 0
-  web_packet = 0
-  for f in event.stats:
-    print(f.duration_sec)
-    #if f.match.tp_dst == 80 or f.match.tp_src == 80:
-    web_bytes += f.byte_count
-    web_packet += f.packet_count
-    web_flows += 1
-  log.info("Traffic from %s: %s bytes (%s packets) over %s flows", 
-    dpidToStr(event.connection.dpid), web_bytes, web_packet, web_flows)
-  """
-
-# Portstats handler can be used to calculate bandwith
-def _handle_portstats_received(event):
-  stats = flow_stats_to_list(event.stats)
-  log.debug("PortStatsReceived from %s: %s", 
-    dpidToStr(event.connection.dpid), stats)
+        #connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
+        #connection.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+    #log.debug("Sent %i flow/port stats request(s)", len(core.openflow._connections))
 
 def _handle_linkevent(event):
     l = event.link
@@ -78,14 +50,16 @@ def _handle_linkevent(event):
 def _add_graph_edge(peer1, peer2, port1, port2, link_type):
     global g
     if link_type == "switch":
-        _gen_link_state_log(peer1, peer2, "Adding new switch-switch edge to graph")
+        _gen_link_state_log("sw"+str(peer1), "sw"+str(peer2), "Adding new switch-switch edge to graph")
         # Edges are initialized with zero values for usage and delay, they'll be computed later
         # state=True means the link is up
-        g.add_edge(peer1, peer2, port1=port1, port2=port2, usage=0, delay=0, state=True)
+        # All switch-switch edges are initialized with a weight of 1.0. This weight value is adjusted as bandwidth and delay calculations run
+        # NOTE: I'm removing the "state" arg because I don't think we'll need it
+        g.add_edge(peer1, peer2, port1=port1, port2=port2, usage=0, delay=0, weight=1.0)
     elif link_type == "host":
         # PEER1 MUST BE THE HOST DO NOT MESS THIS UP
         _gen_link_state_log(peer1, peer2, "Adding new host-switch edge to graph")
-        g.add_edge(peer1, peer2, port1=port1, port2=port2)
+        g.add_edge("h"+str(peer1), "sw"+str(peer2), port1=port1, port2=port2)
 
 def _del_graph_edge(dpid1, dpid2):
     global g
@@ -105,31 +79,69 @@ def _set_graph_edge_state(dpid1, dpid2, state):
     #log.info(str(dpid1) + " -> " + str(dpid2) + ": Updated link state in graph")
 
 def _find_graph_node(attr, val):
-    global g 
     return any([node for node in g.nodes(data=True) if node[1][attr] == val])
 
+def _get_node_id_by_name(node_name):
+    return [node for node in g.nodes(data=True) if node[1]['name'] == node_name]
+
+def _get_node_by_mac_addr(node_mac):
+    for node in g.nodes(data=True):
+        try:
+            # NOTE: Debugging
+            #print(node[1])
+            #print(node_mac)
+            if node[1]['host_mac'] == node_mac:
+                return node
+        except KeyError:    # This exception is hit if we run the check above on a switch node, which doesn't have a host_mac attr
+            continue
+    #return [node for node in g.nodes(data=True) if node[1]['host_mac'] == node_mac]
+
+def _packet_handler(packet, packet_in):
+    pass
+
 def _handle_packetin(event):
-    global g
-    sw_dpid = event.dpid
-    in_port = event.port
     packet = event.parsed
-    
     host_src_mac = str(packet.src)
     host_id = host_src_mac[-1]      # This should not be passed into networkx, this is JUST for getting the name of the node (which corresponds to MAC address)
     host_name = "h" + str(host_id)
-
+        
     # If our host isn't in our graph object, we need to add it
     if not _find_graph_node("name", host_name):
         # Since we need to provide an ID for each node, we can just increment the number of nodes by 1 to get a unique ID
         host_node_id = g.number_of_nodes() + 1
-        g.add_node(host_node_id, name=host_name)
+        g.add_node(host_node_id, name=host_name, host_mac=host_src_mac)
+    else:
+        host_node_id = _get_node_id_by_name(host_name)[0][0]
         
-    _dump_graph_json_data()
+    # Now that we either added a new host node or confirmed it already exists... let's check for edges in the graph
+    host_port = 0 # This will ALWAYS be zero; hosts connect to switches via port 0
+    switch_port = event.port # We can grab this info from the event object
+    sw_dpid = event.dpid
 
-    #if not g.has_node(host_name):
+    # Add an edge between the host and the switch if it doesn't exist already
+    if not g.has_edge(host_node_id, sw_dpid):
+        # NOTE: host_node_id MUST be the first arg here
+        _add_graph_edge(host_node_id, sw_dpid, host_port, switch_port, "host")
+     
+    #packet_dst = packet.dst
 
 
-    
+    #of.ofp_packet_out()
+
+    #if packet_dst == "ff:ff:ff:ff:ff:ff": # If broadcast, flooooooooooooooooooooood
+    #    log.info("Flooding packet")
+    #    msg = of.ofp_packet_out()
+    #    msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+    #    msg.data = event.ofp
+    #    msg.in_port = event.port
+    #    event.connection.send(msg)
+
+    #print(_get_node_by_mac_addr(packet_dst))
+
+
+    #if packet.dst not in 
+    #print(packet_dst)
+    #_dump_graph_json_data()
 
     
     #in_port = event.ofp.in_port
@@ -139,8 +151,8 @@ def _handle_packetin(event):
     #msg = of.ofp_packet_out(data=event.ofp)
     #print(msg)
 
-def _gen_link_state_log(dpid1, dpid2, msg):
-    log.info(str(dpid1) + " -> " + str(dpid2) + ": " + msg)
+def _gen_link_state_log(peer1, peer2, msg):
+    log.info(str(peer1) + " -> " + str(peer2) + ": " + msg)
 
 #def _get_link_delay():
 
@@ -148,6 +160,8 @@ def _dump_graph_json_data():
     global g
     print(json.dumps(json_graph.node_link_data(g),indent=2))
     
+def _handle_queuestats_received(event):
+    print(event.stats)
 
 def launch():
     global g
@@ -156,6 +170,7 @@ def launch():
     core.openflow.addListenerByName("ConnectionUp", _handle_connectionup)
     core.openflow.addListenerByName("FlowStatsReceived", _handle_flowstats_received) 
     core.openflow.addListenerByName("PortStatsReceived", _handle_portstats_received) 
+    core.openflow.addListenerByName("QueueStatsReceived", _handle_queuestats_received)
     core.openflow_discovery.addListenerByName("LinkEvent", _handle_linkevent)
     core.openflow.addListenerByName("PacketIn", _handle_packetin)
 
